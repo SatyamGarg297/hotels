543953ad87f9fc97145eae58d0be08e5feb67ebd
SatyamGarg297 <satyamgarg1672001@gmail.com>
2025-08-05 22:51:09 +0530

    practice jsonwebtoken

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ const personRoutes = require("./routes/personRoutes");
 const MenuItemRoutes = require("./routes/MenuItemRoutes");
 
 // Use the routers
-app.use("/person", localAuthMiddleware, personRoutes);
+app.use("/person", personRoutes);
 app.use("/menu",  MenuItemRoutes);
 
 
